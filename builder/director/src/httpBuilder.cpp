90c65d3b951e6c521673cfba910bcf136a25d2a4
ankit <sahuankit.murhu@gmail.com>
2026-08-20 18:15:14 +0530

    feat(builder): director builder pattern builder impl

---
+++ builder/director/src/httpBuilder.cpp
@@ -0,0 +1,71 @@
+#include "httpBuilder.h"
+#include <stdexcept>
+
+HttpBuilder::HttpBuilder()
+{
+    this->request = new HttpRequest();
+}
+
+HttpBuilder &HttpBuilder::withBody(const std::string body)
+{
+    this->request->body = body;
+    return *this;
+}
+
+HttpBuilder &HttpBuilder::withMethod(const HttpMethod method)
+{
+    switch (method)
+    {
+    case HttpMethod::GET:
+        this->request->method = HttpMethod::GET;
+        break;
+    case HttpMethod::POST:
+        this->request->method = HttpMethod::POST;
+        break;
+    case HttpMethod::PUT:
+        this->request->method = HttpMethod::PUT;
+        break;
+    case HttpMethod::DELETE:
+        this->request->method = HttpMethod::DELETE;
+        break;
+    case HttpMethod::PATCH:
+        this->request->method = HttpMethod::PATCH;
+        break;
+    }
+
+    return *this;
+}
+
+HttpBuilder &HttpBuilder::withHeader(const std::string key, const std::string value)
+{
+    this->request->headers[key] = value;
+    return *this;
+}
+
+HttpBuilder &HttpBuilder::withQueryParam(const std::string key, const std::string value)
+{
+    this->request->queryParams[key] = value;
+    return *this;
+}
+
+HttpBuilder &HttpBuilder::withTimeout(const unsigned int timeout)
+{
+    this->request->timeout = timeout;
+    return *this;
+}
+
+HttpBuilder &HttpBuilder::withUrl(const std::string url)
+{
+    this->request->url = url;
+    return *this;
+}
+
+HttpRequest &HttpBuilder::build()
+{
+    if (this->request->url.empty())
+    {
+        throw std::runtime_error("URL is required to build an HTTP request.");
+    }
+
+    return *this->request;
+}
---
+++ builder/director/src/httpBuilder.h
@@ -0,0 +1,18 @@
+#include "httpRequest.h"
+#pragma once
+
+class HttpBuilder
+{
+private:
+    HttpRequest *request;
+
+public:
+    HttpBuilder();
+    HttpBuilder &withBody(const std::string);
+    HttpBuilder &withMethod(const HttpMethod);
+    HttpBuilder &withHeader(const std::string, const std::string);
+    HttpBuilder &withQueryParam(const std::string, const std::string);
+    HttpBuilder &withTimeout(const unsigned int);
+    HttpBuilder &withUrl(const std::string);
+    HttpRequest &build();
+};
